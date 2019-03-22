89e2e3f0a72aa0c31909972949321ece66146acf
trinhcongduc <38092290+trinhcongduc@users.noreply.github.com>
2019-03-22 21:05:05 +0700

    update

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <!--Content page-->
 <?php include('inc/header.php');?>
 <?php
-echo "<pre>";
-print_r($lang_default);
-echo "</pre>";
 $lang_default == "vi"?include('inc/content-home-vi.php'):include('inc/content-home-en.php')
-
 ?>
 <?php include('inc/footer.php');?>
